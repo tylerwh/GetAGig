775e235bf37d6e803c4f91bcb6c2ad7b76e4f412
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-11-05 14:12:47 -0600

    Initial Commit

---
+++ src/main/java/dmacc/GetAGigApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAGigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAGigApplication.class, args);
+	}
+
+}
